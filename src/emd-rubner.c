817331ffd772ad150a0dd8970fa1bdf9ad72d976
Zachary Fried <friedz@oregonstate.edu>
2018-07-09 11:30:14 -0700

    reset found on line 869 to 0

--- src/emd-rubner.c
+++ src/emd-rubner.c
@@ -866,7 +866,7 @@ static void russel(double *S, double *D)
 #endif
  
       /* FIND THE SMALLEST Delta[i][j] */
-      found = 1; 
+      found = 0; 
       deltaMin = EMD_INFINITY;      
       PrevU = &uHead;
       for (CurU=uHead.Next; CurU != NULL; CurU=CurU->Next)
